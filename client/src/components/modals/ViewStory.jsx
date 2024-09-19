61647e7c277c231008af09c7585e1c19b6771dd2
Rashh <ranjan.rashmi2016@outlook.com>
2024-09-19 14:13:55 +0530

    add video support and ui fixes

--- client/src/components/modals/ViewStory.jsx
+++ client/src/components/modals/ViewStory.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Modal } from "react-responsive-modal";
 import { toast } from "react-toastify";
@@ -7,39 +7,56 @@ import "../../assets/modals/ViewStory.css";
 function ViewStory({ open, onClose }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const slide = parseInt(searchParams.get("slide"), 10) || 0;
+  const videoRef = useRef(null);
 
   const stories = [
     {
       id: 1,
       title: "Heading 1",
       desc: "This description is all about heading 1",
-      img: "https://assets.gadgets360cdn.com/pricee/assets/product/202212/65_1671448856.jpg",
+      media:
+        "https://assets.gadgets360cdn.com/pricee/assets/product/202212/65_1671448856.jpg",
       likes: 105,
     },
     {
       id: 2,
       title: "Heading 2",
       desc: "This description is all about heading 2",
-      img: "https://www.quadrant.io/hs-fs/hubfs/philipp-kammerer-6Mxb_mZ_Q8E-unsplash.jpg",
+      media: "https://beta.testfree.in/demo1.mp4",
       likes: 115,
     },
     {
       id: 3,
       title: "Heading 3",
       desc: "This description is all about heading 3",
-      img: "https://static.toiimg.com/thumb/imgsize-681673,msid-75126749,width-600,height-335,resizemode-75/75126749.jpg",
+      media:
+        "https://static.toiimg.com/thumb/imgsize-681673,msid-75126749,width-600,height-335,resizemode-75/75126749.jpg",
       likes: 125,
     },
     {
       id: 4,
       title: "Heading 4",
       desc: "This description is all about heading 4",
-      img: "https://www.timeshighereducation.com/student/sites/default/files/styles/default/public/istock-499343530.jpg",
+      media:
+        "https://www.timeshighereducation.com/student/sites/default/files/styles/default/public/istock-499343530.jpg",
       likes: 135,
     },
   ];
 
   const [activeSlide, setActiveSlide] = useState(slide);
+  const { id, title, desc, media, likes } = stories[activeSlide];
+
+  const getMediaType = () => {
+    const extension = media.split(".").pop().toLowerCase();
+    const mediaTypes = {
+      image: ["jpg", "jpeg", "png", "gif", "webp", "svg"],
+      video: ["mp4", "mkv", "webm", "ogg", "avi", "mov"],
+    };
+    for (const [type, exts] of Object.entries(mediaTypes)) {
+      if (exts.includes(extension)) return { type, extension };
+    }
+    return "unknown";
+  };
 
   const nextSlide = () => {
     const nextIndex = activeSlide === stories.length - 1 ? 0 : activeSlide + 1;
@@ -66,9 +83,17 @@ function ViewStory({ open, onClose }) {
     } catch (error) {}
   };
 
+  const toggleMediaSound = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !videoRef.current.muted;
+    }
+  };
+
   useEffect(() => {
     if (open) setActiveSlide(slide);
   }, [open, slide]);
+
+  // console.log();
 
   return (
     <Modal
@@ -89,36 +114,41 @@ function ViewStory({ open, onClose }) {
         onClick={prevSlide}
         alt="arrow left"
       />
-      <div
-        className="storyCard"
-        style={{
-          backgroundImage: `url(${stories[activeSlide].img})`,
-        }}
-      >
+      <div className="storyCard">
         <div className="header">
           <div className="slidecount">
             {stories.map((_, idx) => (
               <hr
-                className={`line ${
-                  idx === stories[activeSlide].id - 1 && "active"
-                }`}
+                className={`line ${idx === id - 1 && "active"}`}
                 onClick={() => setActiveSlide(idx)}
                 key={idx}
               />
             ))}
           </div>
           <div className="action">
-            <div>
-              <img src="/icons/x-mark.svg" onClick={onClose} alt="x-mark" />
-            </div>
-            <div>
-              <img src="/icons/send.svg" onClick={shareSlide} alt="share" />
-            </div>
+            <img src="/icons/x-mark.svg" onClick={onClose} alt="x-mark" />
+            {getMediaType().type == "video" && (
+              <img
+                src="/icons/audio.svg"
+                onClick={toggleMediaSound}
+                alt="sound"
+              />
+            )}
+            <img src="/icons/send.svg" onClick={shareSlide} alt="share" />
           </div>
         </div>
+        <div className="media">
+          {getMediaType().type == "image" ? (
+            <img src={media} alt="media" />
+          ) : (
+            <video autoPlay muted loop ref={videoRef}>
+              <source src={media} type={`video/${getMediaType().extension}`} />
+            </video>
+          )}
+        </div>
         <div className="footer">
-          <h3>{stories[activeSlide].title}</h3>
-          <p>{stories[activeSlide].desc}</p>
+          <h3>{title}</h3>
+          <p>{desc}</p>
           <div className="action">
             <div className="bookmark active">
               <svg
@@ -138,7 +168,7 @@ function ViewStory({ open, onClose }) {
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 3c-1.535 0-3.078.5-4.25 1.7-2.343 2.4-2.279 6.1 0 8.5L12 23l9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-.75.8-.75-.8C10.078 3.5 8.536 3 7 3" />
               </svg>
-              <p>{stories[activeSlide].likes}</p>
+              <p>{likes}</p>
             </div>
           </div>
         </div>
